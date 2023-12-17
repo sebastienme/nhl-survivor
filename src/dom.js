20af57aaaf0d883f9dabf345a08d99e5f51ba7a9
sebastien <sebastien.meslage@gmail.com>
2023-12-17 12:59:40 -0500

    add front page loader

--- src/dom.js
+++ src/dom.js
@@ -1,4 +1,21 @@
 
+export const buildDom = (() => {
+    
+    const fadeOutLoader = () => {
+        const loaderContainer = document.getElementById("loader-container");
+        loaderContainer.style.opacity = 0;
+    
+        // After fade out, hide the loader
+        setTimeout(function () {
+            loaderContainer.style.display = "none";
+        }, 1250); // Should match the transition duration in CSS
+    };
+
+    return {
+        fadeOutLoader    
+    }
+})();
+
 export const editDom = (() => {
     const addPlayersCount = (count) => {
         document.querySelector('.header__stats-section__list__item__result').innerHTML = count;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,27 @@ import _ from 'lodash';
 import './dom.js';
 import { dataMethods } from './data.js';
 import './table.js';
+import { buildDom } from './dom.js';
 
 //initialise teams list
 dataMethods.initialiseTeamsList();
 
+//---initiation of loader
+(() => {
+    document.addEventListener("DOMContentLoaded", function () {
+        // Hide loader after 1 seconds
+        setTimeout(function () {
+            buildDom.fadeOutLoader();
+        }, 1250); // Adjust the time as needed
+    });
+})();
+
+//---when animation of loader ends, display none the loader
+(() => {
+    const logo = document.querySelector('.bounceAndShrink'); // Replace with your actual class or element reference
+    logo.addEventListener('animationend', function (event) {
+        if (event.animationName === 'bounceAndShrink') {
+            logo.style.display = 'none';
+        }
+    });
+})();
